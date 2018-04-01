21eee3c971a812b4af66869b20fad8b6dd89fd0b
Pizzanakin <lpizzanakinl@gmail.com>
2018-04-01 12:39:39 +0200

    more app stuff

--- Assets/Scripts/ParentApp Scripts/ObjectiveBehaviour.cs
+++ Assets/Scripts/ParentApp Scripts/ObjectiveBehaviour.cs
@@ -1,23 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ObjectiveBehaviour : MonoBehaviour {
-	public GameObject objectiveScreen;
-	public InputField inputField;
-	public Toggle objectiveObject;
+	public AppBehaviour other;
 
-	public void addNewObjective() {
-		if (inputField.text == "")
-			return;
-		objectiveScreen.SetActive (false);
-		Toggle newObjective = Instantiate (objectiveObject);
-		newObjective.name = "NewObjective";
-		newObjective.transform.SetParent (gameObject.transform);
-		Vector2 position = gameObject.GetComponent<RectTransform> ().anchoredPosition;
-		position.y = 190;
-		newObjective.GetComponent<RectTransform> ().anchoredPosition = position;
-		newObjective.GetComponentInChildren<Text> ().text = inputField.text;
+	void Awake() {
+		other = GameObject.Find ("AppManager").GetComponent<AppBehaviour> ();
+	}
+
+	public void clear() {
+		Destroy(gameObject);
+		other.sortObjectives ();
 	}
 }

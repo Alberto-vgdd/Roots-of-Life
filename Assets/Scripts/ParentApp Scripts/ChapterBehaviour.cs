a4e78f08c85faff7a02e16573b11aba7d472be65
Martyn Zeeman <lpizzanakinl@gmail.com>
2018-04-17 09:44:43 +0200

    app stuff

--- Assets/Scripts/ParentApp Scripts/ChapterBehaviour.cs
+++ Assets/Scripts/ParentApp Scripts/ChapterBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class ChapterBehaviour : MonoBehaviour {
 	public Image textMask;
+	public GameObject nextChapter;
+	public float moveSpeed = 25f;
 	public float waitTime = 10.0f;
 	private int state = 0;
 
@@ -12,24 +14,34 @@ public class ChapterBehaviour : MonoBehaviour {
 	void Update () {
 		if (state == 1) {
 			textMask.fillAmount += 1.0f / waitTime * Time.deltaTime;
-			if (textMask.fillAmount >= 1)
+			Vector2 position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
+			position.y -= moveSpeed;
+			nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
+			if (textMask.fillAmount >= 1) {
 				state = 2;
+				position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
+				position.y = -440;
+				nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
+			}
 		}
 		if (state == 3) {
 			textMask.fillAmount -= 1.0f / waitTime * Time.deltaTime;
-			if (textMask.fillAmount <= 0)
+			Vector2 position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
+			position.y += moveSpeed;
+			nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
+			if (textMask.fillAmount <= 0) {
 				state = 0;
+				position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
+				position.y = -46;
+				nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
+			}
 		}
 	}
 
 	public void press() {
-		float width = 360;
-		float height = textMask.GetComponentInChildren<Text> ().preferredHeight;
-		textMask.rectTransform.sizeDelta = new Vector2 (width, height);
-		Vector2 position = gameObject.GetComponent<RectTransform> ().position;
-		position.x = 0;
-		position.y -= 705;
-		textMask.rectTransform.anchoredPosition = position;
+		//float width = 360;
+		//float height = textMask.GetComponentInChildren<Text> ().preferredHeight;
+		//textMask.rectTransform.sizeDelta = new Vector2 (width, height);
 
 		if (state == 0)
 			state = 1;

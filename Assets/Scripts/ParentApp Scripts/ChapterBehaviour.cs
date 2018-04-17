ac4104d91fc824ed7a01d935937d05a0f2e8f62d
Pizzanakin <lpizzanakinl@gmail.com>
2018-04-17 07:51:03 +0200

    Revert "app updates"
    This reverts commit 7dcc5a20406a521fc50261fa720b6c436bb895c7.

--- Assets/Scripts/ParentApp Scripts/ChapterBehaviour.cs
+++ Assets/Scripts/ParentApp Scripts/ChapterBehaviour.cs
@@ -5,32 +5,32 @@ using UnityEngine.UI;
 
 public class ChapterBehaviour : MonoBehaviour {
 	public Image textMask;
-	public GameObject nextChapter;
 	public float waitTime = 10.0f;
-	public float movespeed = 32f;
 	private int state = 0;
 
 	// Update is called once per frame
 	void Update () {
 		if (state == 1) {
 			textMask.fillAmount += 1.0f / waitTime * Time.deltaTime;
-			Vector2 position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
-			position.y -= movespeed;
-			nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
 			if (textMask.fillAmount >= 1)
 				state = 2;
 		}
 		if (state == 3) {
 			textMask.fillAmount -= 1.0f / waitTime * Time.deltaTime;
-			Vector2 position = nextChapter.GetComponent<RectTransform> ().anchoredPosition;
-			position.y += movespeed;
-			nextChapter.GetComponent<RectTransform> ().anchoredPosition = position;
 			if (textMask.fillAmount <= 0)
 				state = 0;
 		}
 	}
 
 	public void press() {
+		float width = 360;
+		float height = textMask.GetComponentInChildren<Text> ().preferredHeight;
+		textMask.rectTransform.sizeDelta = new Vector2 (width, height);
+		Vector2 position = gameObject.GetComponent<RectTransform> ().position;
+		position.x = 0;
+		position.y -= 705;
+		textMask.rectTransform.anchoredPosition = position;
+
 		if (state == 0)
 			state = 1;
 		if (state == 2)

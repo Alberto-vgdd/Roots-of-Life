c8890e4d2e49097dee23e05c4751d629f42ad8c2
oscar <oscar.morehead@gmail.com>
2018-04-17 09:19:58 +0200

    end screen + fog + other stuff

---
+++ Assets/Scripts/FogTextScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FogTextScript : MonoBehaviour {
+
+    public Text text1;
+    public GameObject fogText;
+	// Use this for initialization
+	void Start () {
+        text1 = GetComponent<Text>();
+        
+        //text1.enabled = false;  
+	}
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //text1.enabled = true;
+            fogText.gameObject.SetActive(true);
+            Debug.Log("whyaintuworkin");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}

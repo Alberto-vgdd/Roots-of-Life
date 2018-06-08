68843f68c9f8b30c0b5a7c37e30a4a898f23f1e4
Pizzanakin <lpizzanakinl@gmail.com>
2018-06-08 22:04:26 +0200

    Implemented app intro scene + improved flag listener behaviour

--- Assets/Scripts/ExampleCube.cs
+++ Assets/Scripts/ExampleCube.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ExampleCube : MonoBehaviour {
-    public FlagListener listener;
+    private  FlagListener listener;
 	// Use this for initialization
+
 	void Start () {
-		
+        listener = GameObject.Find("[X] Flag Listener").GetComponent<FlagListener>();
 	}
 	
 	// Update is called once per frame
@@ -16,7 +17,7 @@ public class ExampleCube : MonoBehaviour {
 
     public void onEvent()
     {
-        int value = listener.getValue();
+        int value = listener.getValue("objectVanish");
         if (value == 0)
             gameObject.SetActive(false);
         if (value == 1)
--- Assets/Scripts/FlagListener.cs
+++ Assets/Scripts/FlagListener.cs
@@ -2,37 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Linq;
 
 public class FlagListener : MonoBehaviour {
     string URL = "http://143.176.117.92/roots-of-life/getFlags.php";
-    public string flag;
-    public int valueReadOnly;
-    protected int value;
-    protected string[] flagData;
 
-    public UnityEvent flagValueChange;
+    public List<string> flagnames;
+    public List<UnityEvent> events;
+    public List<int> valuesReadOnly;
+
+    private string[] flagData;
+    private Dictionary<string, int> flags;
 
 	// Use this for initialization
 	void Start () {
+        flags = new Dictionary<string, int>();
+        foreach (string name in flagnames)
+            flags.Add(name, 0);
+    }
+
+    private void OnValidate()
+    {
+        // Set events list size equal to flag names size, but save the data in the list
+        if (events.Count != flagnames.Count)
+        {
+            UnityEvent[] newArray = new UnityEvent[flagnames.Count];
+            for (int i = 0; i < flagnames.Count; i++)
+            {
+                if (events.Count <= i)
+                    continue;
+                newArray[i] = events[i];
+            }
+            events = new List<UnityEvent>(newArray);
+        }
+        valuesReadOnly = new List<int>(new int[flagnames.Count]);
     }
 
     IEnumerator loadFlags()
     {
-        WWW flags = new WWW(URL);
-        yield return flags;
-        string flagsDataString = flags.text.TrimEnd(';');
+        WWW flagloader = new WWW(URL);
+        yield return flagloader;
+        string flagsDataString = flagloader.text.TrimEnd(';');
         flagData = flagsDataString.Split(';');
+        
+        // Iterate over all flag values obtained from url
         foreach (string data in flagData)
         {
-            //Debug.Log(data);
+            // Store flag name and flag value
             string name = data.Split(',')[0];
-            string sValue = data.Split(',')[1];
-            if (name == flag)
+            int value = int.Parse(data.Split(',')[1]);
+
+            // Check if flag is checked for
+            if (flags.ContainsKey(name))
             {
-                int oldValue = value;
-                value = int.Parse(sValue);
-                if (oldValue != value)
-                    onFlagChange();
+                int i = flagnames.IndexOf(name);
+
+                // Check for value update
+                if (flags[name] != value)
+                    events[i].Invoke();
+
+                // Update value
+                flags[name] = value;
+                valuesReadOnly[i] = value;
             }
         }
     }
@@ -40,17 +71,12 @@ public class FlagListener : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         StartCoroutine(loadFlags());
-        valueReadOnly = value;
 	}
 
-    void onFlagChange()
+    public int getValue(string flag)
     {
-        Debug.Log("event called");
-        flagValueChange.Invoke();
-    }
-
-    public int getValue()
-    {
-        return value;
+        if (!flags.ContainsKey(flag))
+            return -1;
+        return flags[flag];
     }
 }

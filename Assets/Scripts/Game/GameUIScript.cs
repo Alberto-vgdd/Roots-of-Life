7ad7984092a7bef7c01f2d605af0b0f5f0bc2e63
Alberto <alberto.vgdd@gmail.com>
2018-06-15 09:49:09 +0200

    UI messages working!

--- Assets/Scripts/Game/GameUIScript.cs
+++ Assets/Scripts/Game/GameUIScript.cs
@@ -1,27 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameUIScript : MonoBehaviour
 {
 	private Animator animator;
 	public Text acornCounter;
-	public GameObject message1;
+	public Text messageText;
 
 	private const string gameFadeOut = "GameFadeOut";
 	private const string gameFadeIn = "GameFadeIn";
-	
+	private const string displayMessage = "DisplayMessage";
+
+	private Coroutine displayMessageCoroutine;
+	private float displayMessageTime = 3.5f;
+	private Queue<string> messages;
+
+
 	// Use this for initialization
 	void Awake ()
 	{
 		GlobalData.GameUIScript = this;
 		animator = GetComponent<Animator>();
+		messages = new Queue<string>();
 	}
 
 	void Start()
 	{	
 		UpdateAcornCounter();
+
+		//Testing message
+		DisplayMessage(SceneManager.GetActiveScene().name);
 	}
 	
 
@@ -38,6 +49,34 @@ public class GameUIScript : MonoBehaviour
 	public void UpdateAcornCounter()
 	{
 		acornCounter.text = "" + GlobalData.AcornCount;
+	}
+
+	IEnumerator DisplayMessage()
+	{
+		while (messages.Count > 0)
+		{
+			string message = messages.Dequeue();
+			messageText.text = message;
+			animator.SetTrigger(displayMessage);
+
+			yield return new WaitForSeconds(displayMessageTime);
+		}
+		
+		StopCoroutine(displayMessageCoroutine);
+		displayMessageCoroutine = null;
+
+		yield return null;
+	}
+
+
+	public void DisplayMessage(string messageText)
+	{
+		messages.Enqueue(messageText);
+		
+		if (displayMessageCoroutine == null)
+		{
+			displayMessageCoroutine = StartCoroutine(DisplayMessage());
+		}
 	}
 
 }

9f9173c80ceddd89b87bf851b346cd9198012a3a
Alberto <alberto.vgdd@gmail.com>
2018-06-16 20:17:33 +0200

    Added Breakable object

--- Assets/Scripts/Game/Environment Scripts/Interactable Objects/BreakableObject.cs
+++ Assets/Scripts/Game/Environment Scripts/Interactable Objects/BreakableObject.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class BreakableObject : MonoBehaviour, IInteractable
 {
+	private GameObject parentGameObject; 
+
+	void Start()
+	{
+		parentGameObject = transform.parent.gameObject;
+	}
+
 
 	public void OnPush()
 	{
-		Debug.Log("Hit!");
+		GlobalData.GameManager.ShakeCamera(0.25f,0.4f);
+		parentGameObject.SetActive(false);
 	}
 
 	public void OnPull()

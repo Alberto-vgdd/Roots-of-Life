5e5fadd936a35af48783c8f84072eb254fe6f04b
Pizzanakin <lpizzanakinl@gmail.com>
2018-06-17 15:03:01 +0200

    polished game/app communication

--- Assets/Scripts/Game/PlayDetector.cs
+++ Assets/Scripts/Game/PlayDetector.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 
 public class PlayDetector : MonoBehaviour {
 
-    private string URL = "http://62.131.170.46/roots-of-life/setActive.php";
+    private string activeURL = "http://62.131.170.46/roots-of-life/setActive.php";
+    private string inactiveURL = "http://62.131.170.46/roots-of-life/setInactive.php";
 
-    public Text usernameText;
+    public string username;
+
+    private void Awake()
+    {
+        if (GetComponent<FlagListener>() != null)
+            username = GetComponent<FlagListener>().username;
+    }
 
     // Use this for initialization
     void Start () {
@@ -22,10 +29,11 @@ public class PlayDetector : MonoBehaviour {
 
     IEnumerator setActive()
     {
+        Debug.Log(username);
         WWWForm form = new WWWForm();
-        form.AddField("setUsername", usernameText.text);
+        form.AddField("setUsername", username);
         form.AddField("setLogin", (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
-        WWW www = new WWW(URL, form);
+        WWW www = new WWW(activeURL, form);
         yield return www;
 
         if (!string.IsNullOrEmpty(www.error))
@@ -38,8 +46,33 @@ public class PlayDetector : MonoBehaviour {
         }
     }
 
-    public void processPlay()
+    IEnumerator setInactive()
     {
+        WWWForm form = new WWWForm();
+        form.AddField("setUsername", username);
+        form.AddField("setLogin", (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+        WWW www = new WWW(inactiveURL, form);
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log("error: " + www.error);
+        }
+        else
+        {
+            Debug.Log("result: " + www.text);
+        }
+    }
+
+    public void startPlay()
+    {
+        Debug.Log("test1");
         StartCoroutine(setActive());
+    }
+
+    public void stopPlay()
+    {
+        if (username != null && username != "")
+            StartCoroutine(setInactive());
     }
 }

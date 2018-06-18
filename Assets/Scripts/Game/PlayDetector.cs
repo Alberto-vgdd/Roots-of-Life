4b42d66ceea0b5cabd2722b552b41d976906a102
Pizzanakin <lpizzanakinl@gmail.com>
2018-06-18 17:44:29 +0200

    implemented player activity tracker

--- Assets/Scripts/Game/PlayDetector.cs
+++ Assets/Scripts/Game/PlayDetector.cs
@@ -8,8 +8,10 @@ public class PlayDetector : MonoBehaviour {
 
     private string activeURL = "http://62.131.170.46/roots-of-life/setActive.php";
     private string inactiveURL = "http://62.131.170.46/roots-of-life/setInactive.php";
+    private string pingURL = "http://62.131.170.46/roots-of-life/userPing.php";
 
     public string username;
+    public bool liveUpdate;
 
     private void Awake()
     {
@@ -23,7 +25,16 @@ public class PlayDetector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (liveUpdate)
+            StartCoroutine(pingTimer());
+    }
 
+    IEnumerator pingTimer()
+    {
+        liveUpdate = false;
+        StartCoroutine(ping());
+        yield return new WaitForSeconds(60);
+        liveUpdate = true;
     }
 
     public void setUsername(string username)
@@ -68,6 +79,24 @@ public class PlayDetector : MonoBehaviour {
         }
     }
 
+    IEnumerator ping()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("setUsername", username);
+        form.AddField("setPing", (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+        WWW www = new WWW(pingURL, form);
+        yield return www;
+
+        if (!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log("error: " + www.error);
+        }
+        else
+        {
+            Debug.Log("result: " + www.text);
+        }
+    }
+
     public void startPlay()
     {
         StartCoroutine(setActive());
@@ -77,5 +106,11 @@ public class PlayDetector : MonoBehaviour {
     {
         if (username != null && username != "")
             StartCoroutine(setInactive());
+    }
+
+    public void stopPlay(string username)
+    {
+        this.username = username;
+        StartCoroutine(setInactive());
     }
 }
